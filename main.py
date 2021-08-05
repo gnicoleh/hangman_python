9ae7a9e45ed214f9508f92a237204b7db429f550
NightmareScout <NightmareScout@github.com>
2021-08-05 19:20:35 -0400

    Added comments on most functions, santized all inputs to correct lower or upper case forms

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import random
 word_bank = ['chocolate', 'Horus Heresy', 'heretic', 'The God Emperor', 'Terraria', 'Siege', 'Beaulo', 'Dark Souls', 'cottoncandy', 'cryptocurrency', 'sparkpoint', 'PayMoneyWubby', 'SweetAnita']
 
 ## Choosing a random word from a list
-
 def choose_word(word_bank):
-    return random.choice(word_bank)
+    return str(random.choice(word_bank)) #need to ensure all outputs are strings for comparison
 
+#For testing purposes, word chosen is currently printed out, will be removed in the future
 word = choose_word(word_bank)
-print(word)
+display_word = word
+print(display_word)
+
+#we save the word as all lowercase so that it does not matter if player inputs capitals or not
+word = word.lower()
 
 ## Creating blank spaces for each character in the chosen word
 def word_to_guess(word):
@@ -27,11 +31,14 @@ blanks = word_to_guess(word)
 print(*blanks, sep='')
 print(" ")
 
+#main functionality of the game
 def start_game (word, blanks):
 	attempts = 6
 	while True:
 		if "_" in blanks:
-			given_letter = input("Please enter the character that you think is in the word(s)\n")
+			given_letter = input("Please enter a character that you think is in the word(s)\n")
+			given_letter = given_letter.lower() #sanitize all inputs to also be lowercase so everything matches
+			#safety check to ensure only 1 character entered at a time
 			if len(given_letter) > 1:
 				print("Please enter only one character at a time.")
 				continue
@@ -57,21 +64,22 @@ def start_game (word, blanks):
 			break
 	return None
 
-
+#function to repeat the game if the player wants to
 def play_again():
 	while True:
-		again = input("Would you like to play again?\nY\nN\n")
+		again = input("Would you like to play again? Y/N: ")
+		again = again.upper() #pull input into uppercase to match if-else statement below
 		if again == "Y":
-			break
+			break #breaks from the loop, allowing function to return true, allowing gameplay loop to continue, right back to startgame
 		elif again == "N":
-			print("Ok, see you next time!")
+			print("Ok, see you next time!") #later nerd
 			raise SystemExit		## Does the same thing 'Sys.exit()' does, but doesn't import the sys package
 		else:
 			print("Please choose either 'Y' or 'N'")
 			continue
 	return True
 
-
+#main gameplay loop
 while True:
 	start_game(word, blanks)
 	if play_again() == True:
