1ec85efbbeed8bc49a8e204a26ddaab2633c3f81
gnicoleh <guisella.n.hernandez@gmail.com>
2021-08-05 19:02:14 -0400

    Added line to check if letter is in the chosen word

--- main.py
+++ main.py
@@ -35,23 +35,24 @@ def start_game (word, blanks):
 			if len(given_letter) > 1:
 				print("Please enter only one character at a time.")
 				continue
-			for char in word:
-				if char == given_letter:
-					position_of_letter = word.index(given_letter)
-					blanks[position_of_letter] = given_letter
-					print(blanks)
-				elif attempts > 2:
-					attempts -= 1
-					print("Oops! Looks like that character is not in the word(s). Try again! You have " + str(attempts) + " attempts remaining.")
-					continue
-				elif attempts == 2:
-					attempts -= 1
-					print("Oops! Looks like that character is not in the word(s). Try again! This is your final attempt!")
-					continue
-				else:
-					attempts -= 1
-					print("Oops! Looks like that character is not in the word(s). You have ran out of attempts, better luck next time!")
-					break
+			if given_letter in word:
+				for char in word:
+					if char == given_letter:
+						position_of_letter = word.index(given_letter)
+						blanks[position_of_letter] = given_letter
+						print(blanks)
+			elif attempts > 2:
+				attempts -= 1
+				print("Oops! Looks like that character is not in the word(s). Try again! You have " + str(attempts) + " attempts remaining.")
+				continue
+			elif attempts == 2:
+				attempts -= 1
+				print("Oops! Looks like that character is not in the word(s). Try again! This is your final attempt!")
+				continue
+			else:
+				attempts -= 1
+				print("Oops! Looks like that character is not in the word(s). You have ran out of attempts, better luck next time!")
+				break
 		else:
 			print("Huzzah! You guessed ' " + blanks + "' right! Thank you for playing, see you next time!")
 			break
