9caa713d92e9cb62bad6afe6eca1bdc17e865d4f
gnicoleh <guisella.n.hernandez@gmail.com>
2021-08-09 18:33:25 -0400

    Started working on the gui

---
+++ gui.py
@@ -0,0 +1,23 @@
+## GUI FOR HANGMAN
+
+from tkinter import *
+from tkinter import ttk
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+# Setting up the Main Application Window
+root = Tk()
+root.title("Hangman")
+
+# Creating a Content Frame
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+# Creating the Entry Widget
