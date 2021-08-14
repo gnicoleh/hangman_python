8d5393e0a3d5f3c074bd47f6420d7da9088470ba
NightmareScout <NightmareScout@github.com>
2021-08-14 18:58:45 -0400

    Added volume button and overall structure of function based program execution

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@
 from tkinter import *
 #from tkinter import ttk
 import tkinter.font as font
-import webbrowser
+import webbrowser, winsound, os
 
 gamepageurl = "https://github.com/gnicoleh/hangman_python" # make repo "public" so it works for everyone
 def openweb():
@@ -27,27 +27,71 @@ def openNewWindow():
     Label(newWindow, text="This is the game window").pack()
 
 
+def SoundButtonOff(button):
+    button.image = volumeOffImage
+
+def SoundButtonOn():
+    pass
+
+
+def DrawMainWindow():
+    for widgets in mainWindow.winfo_children():
+        widgets.destroy()
+
+    titlebar = Label(mainWindow, bg="black", fg="green", text="CRYPTO \nHANGMAN", font=(myFont, 62, "bold")).place(relx=0.5, rely=0.2, anchor=CENTER)
+    humorbar = Label(mainWindow, bg="black", fg="green", font=(myFont, 12, "italic"), text="An exercise in infuriating coding brought to you by Nicole and Zach").place(relx=0.5, rely=0.3, anchor=CENTER)
+
+    # add widgets here
+    button1 = Button(mainWindow, bg="green", fg="white", font=myFont, text="Play", command=PlayGame).place(relx=0.5, rely=0.5, anchor=CENTER)
+    button2 = Button(mainWindow, bg="green", fg="white", font=myFont, text="Settings", command=OpenSettings).place(relx=0.5, rely=0.6, anchor=CENTER)
+    button3 = Button(mainWindow, bg="green", fg="white", font=myFont, text="Github Code", command= openweb).place(relx=0.5, rely=0.7, anchor=CENTER)
+    button4 = Button(mainWindow, bg="green", fg="white", font=myFont, text="Quit", command= mainWindow.destroy).place(relx=0.5, rely=0.8, anchor=CENTER)
+
+    soundbutton = Button(mainWindow, image= volumeOnImage, height=50, width=50).place(relx=0.9, rely=0.9, anchor=CENTER)
+
+    mainWindow.mainloop()
+
+    
+
+def PlayGame():
+    for widgets in mainWindow.winfo_children():
+        widgets.destroy()
+
+    soundbutton = Button(mainWindow, image= volumeOnImage, height=50, width=50).place(relx=0.9, rely=0.9, anchor=CENTER)
+
+def OpenSettings():
+    for widgets in mainWindow.winfo_children():
+        widgets.destroy()
+    
+    titlebar = Label(mainWindow, bg="black", fg="green", text="SETTINGS", font=(myFont, 62, "bold")).place(relx=0.5, rely=0.2, anchor=CENTER)
+    button1 = Button(mainWindow, bg="green", fg="white", font=myFont, text="Back", command=DrawMainWindow).place(relx=0.5, rely=0.5, anchor=CENTER)
+    soundbutton = Button(mainWindow, image= volumeOnImage, height=50, width=50).place(relx=0.9, rely=0.9, anchor=CENTER)
+
+
+
 # Tk object which will be treated as a new window
 mainWindow = Tk()
-# sets the title of the window widget
-mainWindow.title("Crypto Hangman")
-# set upper-left icon using mainWindow.iconbitmap() || use .ico for windows os and .xbm for linux os
-# set the geometry of the window
+mainWindow.title("Crypto Hangman") # sets the title of the window widget
 width = mainWindow.winfo_screenwidth()
-height = mainWindow.winfo_screenheight()
-mainWindow.geometry("%dx%d" % (width, height))
+height = mainWindow.winfo_screenheight() 
+mainWindow.geometry("%dx%d" % (width, height)) # set the geometry of the window
 mainWindow.configure(bg="black")
+# set upper-left icon using mainWindow.iconbitmap() || use .ico for windows os and .xbm for linux os
 
-# define font
-myFont = font.Font(family="Segoe UI", size=30, weight="bold")
+myFont = font.Font(family="Segoe UI", size=30, weight="bold") # define font
 
-titlebar = Label(mainWindow, bg="black", fg="green", text="CRYPTO \nHANGMAN", font=(myFont, 62, "bold")).place(relx=0.5, rely=0.2, anchor=CENTER)
-humorbar = Label(mainWindow, bg="black", fg="green", font=(myFont, 12, "italic"), text="An exercise in infuriating coding brought to you by Nicole and Zach").place(relx=0.5, rely=0.3, anchor=CENTER)
+#plays the soundtrack
+soundfile_path = os.path.dirname(os.path.abspath(__file__)) + '\zeldas_lullaby_piano.wav'
+soundfile = soundfile_path.replace("\\","/")
+winsound.PlaySound(soundfile, winsound.SND_ASYNC + winsound.SND_LOOP) #SND_ASYNC allows other process to run cocurrently once the sound file starts. SND_LOOP will loop the song until the program is closed
 
-# add widgets here
-button1 = Button(mainWindow, bg="green", fg="white", font=myFont, command=openNewWindow, text="Play").place(relx=0.5, rely=0.5, anchor=CENTER)
-button2 = Button(mainWindow, bg="green", fg="white", font=myFont, text="Settings").place(relx=0.5, rely=0.6, anchor=CENTER)
-button3 = Button(mainWindow, bg="green", fg="white", font=myFont, command= openweb, text="Github Code").place(relx=0.5, rely=0.7, anchor=CENTER)
-button4 = Button(mainWindow, bg="green", fg="white", font=myFont, command= mainWindow.destroy, text="Quit").place(relx=0.5, rely=0.8, anchor=CENTER)
+folderpath = os.path.dirname(os.path.abspath(__file__))
+tempvolumeOnPath = folderpath + '\\volumeon.gif'
+tempvolumeOffPath = folderpath + '\\volumeoff.gif'
+volumeOnPath = tempvolumeOnPath.replace("\\","/")
+volumeOffPath = tempvolumeOffPath.replace("\\","/")
+volumeOnImage = PhotoImage(file= volumeOnPath)
+volumeOffImage = PhotoImage(file= volumeOffPath)
 
-mainWindow.mainloop()
+while TRUE:
+    DrawMainWindow()
